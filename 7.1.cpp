274aaba64913bc284e9fdeb8434bc5f57a9105d5
Muhammad Taha <127325759+MuhammadTaha01@users.noreply.github.com>
2023-04-05 02:10:22 +0500

    Add files via upload

---
+++ 7.1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    cout<<"Enter five integrs: "<<endl;
+    cin>>arr[0];
+    cin>>arr[1];
+    cin>>arr[2];
+    cin>>arr[3];
+    cin>>arr[4];
+
+    cout<<"The value in arrays are: "<<endl;
+    cout<<arr[0]<<endl;
+    cout<<arr[1]<<endl;
+    cout<<arr[2]<<endl;
+    cout<<arr[3]<<endl;
+    cout<<arr[4]<<endl;
+
+    return 0;
+}
